ec950ebbbd1db7da2bfa47ef33e4a4de4a30245f
Gert Meulyzer <cel357@gmail.com>
2025-07-29 11:49:37 +0200

    new version of setOutput
    The old way of working is being deprecated.
    See:
    https://github.blog/changelog/2022-10-11-github-actions-deprecating-save-state-and-set-output-commands/

--- action.go
+++ action.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log/slog"
+	"os"
+)
 
-func setOutput(name string, value string) {
-	fmt.Printf("::set-output name=%s::%s", name, value)
+func setOutput(name, value string) {
+	f, err := os.OpenFile(os.Getenv("GITHUB_OUTPUT"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		slog.Error("cannot open GITHUB_OUTPUT for writing", "error", err)
+		return
+	}
+	_, err = fmt.Fprintf(f, "{%s}={%s}", name, value)
+	if err != nil {
+		slog.Error("cannot write to GITHUB_OUTPUT", "error", err)
+	}
 }
